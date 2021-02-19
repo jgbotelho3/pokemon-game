77c09180e8821bc5d4da47e7dfb65c65890c1853
Jefferson <jgbotelho3@outlook.com>
2021-02-18 21:53:21 -0300

    First commit in development

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Nav />
+    <Main />
+    <div class="relative">
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Main,
+    Footer,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* background-image: url('./assets/bg2.jpg');
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh;
+  background-position: center center;
+  background-size: contain; */
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,83 @@
+<template>
+  <main class="mx-auto w-full h-full">
+    <div class="flex bg-gray-50 rounded-md shadow-2xl max-w-screen-xl mx-auto">
+      <div class="w-1/2 h-full py-40 max-w-screen-md">
+        <img
+          class="w-1/2 mx-auto"
+          src="../assets/logo.png"
+          width="200px"
+          height="200px"
+          alt="Logo Vue JS"
+        />
+
+        <div
+          class="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 h-20 flex justify-items-center items-center shadow-2xl rounded-md"
+        >
+          <h1
+            class="italic mx-auto text-4xl font-bold text-white text-center px-4"
+          >
+            Quem é este pokemon????
+          </h1>
+        </div>
+      </div>
+      <div
+        class="w-1/2 h-full py-40 flex flex-col items-center justify-items-center"
+      >
+        <button
+          class="mt-4 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 w-4/5 h-16 flex justify-items-center items-center shadow-2xl rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+        >
+          <span class="mx-auto text-2xl font-bold text-white text-center px-4">
+            Option 1
+          </span>
+        </button>
+
+        <button
+          class="mt-4 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 w-4/5 h-16 flex justify-items-center items-center shadow-2xl rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+        >
+          <span class="mx-auto text-2xl font-bold text-white text-center px-4">
+            Option 1
+          </span>
+        </button>
+
+        <button
+          class="mt-4 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 w-4/5 h-16 flex justify-items-center items-center shadow-2xl rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+        >
+          <span class="mx-auto text-2xl font-bold text-white text-center px-4">
+            Option 1
+          </span>
+        </button>
+
+        <button
+          class="mt-4 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 w-4/5 h-16 flex justify-items-center items-center shadow-2xl rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+        >
+          <span class="mx-auto text-2xl font-bold text-white text-center px-4">
+            Option 1
+          </span>
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+
+export default {
+  name: "Main",
+  data() {
+    return {
+      data: {},
+    };
+  },
+  props: {
+    pokemon: {},
+  },
+  setup() {
+    onMounted( () => {
+      this.$store.dispatch('storePokemon');
+       console.log(this.$store.state.pokemons);
+    });
+  },
+};
+</script>
